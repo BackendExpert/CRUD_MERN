bf22f922d2e8e1ba412ea66344e8f18980d82c73
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 05:45:44 +0530

    Updaet
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Users from "./User/Users";
+import CreateUser from "./User/CreateUser";
+import UpdateUser from "./User/UpdateUser";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/User" element={}></Route>
-        <Route path="/User/create" element={}></Route>
-        <Route path="/User/update" element={}></Route>
+        <Route path="/User" element={<Users />}></Route>
+        <Route path="/User/create" element={<CreateUser />}></Route>
+        <Route path="/User/update" element={<UpdateUser />}></Route>
       </Routes>
     </BrowserRouter>
   )
