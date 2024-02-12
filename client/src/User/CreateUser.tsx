ac6f2a668ea5dc95cef7b4dfdfacbb64dc598956
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 06:08:53 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- client/src/User/CreateUser.tsx
+++ client/src/User/CreateUser.tsx
@@ -2,9 +2,19 @@
 const CreateUser = () => {
   return (
     <div className="bg-blue-500 w-full h-screen">
-        <div className="pt-12 h-auto">
-            <div className="py-4">
-                hi all
+        <div className="container mx-auto px-10">
+            <div className="pt-12 h-auto">
+                <div className="px-8 py-8 bg-white rounded">
+                    <p className="text-xl font-semibold">Add New User</p>
+                    <hr className="mb-8" />
+
+                    <form>
+                        <div className="my-4 mx-6">
+                            <label htmlFor="">Username : </label>
+                            <input type="text" name="" id="" className="w-full h-12"/>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
