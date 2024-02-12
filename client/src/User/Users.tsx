ed7af13a656bfbfd4610cef48b15012c3228e390
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 05:50:31 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Vinura Pathirana <154809219+vinurapathirana@users.noreply.github.com>

--- client/src/User/Users.tsx
+++ client/src/User/Users.tsx
@@ -1,7 +1,29 @@
 
 const Users = () => {
   return (
-    <div>Users</div>
+    <div className="bg-blue-500 h-screen w-full">
+        <div className="container mx-auto px-10">
+            <div className="pt-10 h-auto">
+                <div className="px-12 py-8 bg-white rounded">
+                    <p className="text-2xl font-semibold">Users</p>
+                    <hr className="pb-8"/>
+
+                    <table className="table-fixed">
+                        <thead>
+                            <tr>
+                                <td>Name</td>
+                                <td>Email</td>
+                                <td>Age</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
